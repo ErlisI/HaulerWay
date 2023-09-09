386c9c8aefef8588da6dcb0f198b25803703f4ea
Erlis Ibraimi <erloibri1@gmail.com>
2023-09-09 19:22:22 -0400

    on the server side now you can enter addresses and get a route as coordinates

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,26 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 4000;
-const session = require("express-session");
-require("dotenv").config();
-const cors = require("cors");
+const session = require('express-session');
+require('dotenv').config();
+const cors = require('cors');
 const {
   forbiddenErrorHandler,
   notFoundErrorHandler,
-} = require("./middleware/errorHandlers");
+} = require('./middleware/errorHandlers');
+const navigationRoutes = require('./routes/navigation');
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    allowedHeaders: ["Content-Type", "Authorization"],
-    methods: ["GET", "POST", "PATCH", "DELETE"]
+    origin: 'http://localhost:5173',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   })
 );
 
 app.use((req, res, next) => {
   console.log(`Request: ${req.method} ${req.originalUrl}`);
-  res.on("finish", () => {
-    // the 'finish' event will be emitted when the response is handed over to the OS
+  res.on('finish', () => {
     console.log(`Response Status: ${res.statusCode}`);
   });
   next();
@@ -28,10 +28,10 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
+app.use('/', navigationRoutes);
+
 app.use(forbiddenErrorHandler);
 app.use(notFoundErrorHandler);
-
-
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
